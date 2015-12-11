3b8fa8bacf9f6bb69afaa8f0fe8ef339f550a559
Fran <fabrilalvarez@danielcastelao.org>
2015-12-11 12:17:17 +0100

    Añadido el id del boton en el fichero xml fragment_objeto_detail.xml En El fragment del layout hemos instanciado el boton y le damos una función mediante el metodo onClick Cuando se pulse el botón siempre borrara el texto pero en caso de que la aplicación este en PORT se cerrará la activity.

--- app/src/main/java/com/example/fran/examenpmdm/objetoDetailFragment.java
+++ app/src/main/java/com/example/fran/examenpmdm/objetoDetailFragment.java
@@ -1,13 +1,18 @@
 package com.example.fran.examenpmdm;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fran.examenpmdm.dummy.DummyContent;
 
@@ -57,12 +62,31 @@ public class objetoDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_objeto_detail, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_objeto_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.objeto_detail)).setText(mItem.details);
         }
+
+        /**
+         * El btnBorrar elimina el texto que hay
+         */
+        Button btnBorrar = (Button) rootView.findViewById(R.id.btnBorrar);
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)//construye isInLayout para la versión 4.2
+            @Override
+            public void onClick(View v) {
+                //SE REEMPLAZA EL ITEM POR UN TEXT SIN NADA
+                ((TextView) v.findViewById(R.id.objeto_detail)).setText("");
+                //EN CASO QUE LA APLICACION ESTÉ EN PORT SE CERRARA EL ACTIVITY, SI NO, NO HACE NADA
+                if (rootView == null || !rootView.isInLayout()) {//En la versión 4.2 no me permite hacer isInLayout
+                    getActivity().finish();
+                } else {
+
+                }
+            }
+        });
 
         return rootView;
     }
